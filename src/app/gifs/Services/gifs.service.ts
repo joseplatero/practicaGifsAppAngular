53a704b16d882bb523911d09462315e5e5539b97
joseplatero <albert_mirand@yahoo.es>
2022-11-17 17:25:30 -0600

    Search gifs with Angular practise/ buscar gifs con angular terminado

--- src/app/gifs/Services/gifs.service.ts
+++ src/app/gifs/Services/gifs.service.ts
@@ -1,63 +1,121 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpParams } from '@angular/common/http';
+// import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
+
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class GifsService {
+  
+//   private apiKey      : string = 'dr0k4vzB3ayXZPoj6opbHsWkk5JKMXf9';
+//   private servicioUrl : string = 'https://api.giphy.com/v1/gifs';
+//   private _historial  : string [] = [];
+
+//   public resultados: Gif [] = [];
+
+
+//   get historial() {
+//     return [...this._historial];
+//   }
+
+//   constructor( private http : HttpClient){
+
+//     this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+//     this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+
+
+//     // if(localStorage.getItem('historial')){
+//     //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+//     // }
+
+//   }
+
+
+
+//   buscarGifs(query : string = '' ){
+
+//     query = query.trim().toLocaleLowerCase();
+
+//     if( !this._historial.includes (query) ){
+//       this._historial.unshift( query ) ;
+//       this._historial = this._historial.splice(0,10);
+
+//       localStorage.setItem( 'historial', JSON.stringify(this._historial));
+//     }
+    
+//     const params = new HttpParams()
+//           .set('api_Key', this.apiKey)
+//           .set('limit', '10')
+//           .set('q', query);
+
+//     this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params })
+//       .subscribe( ( resp ) => {
+//         this.resultados = resp.data;
+//         localStorage.setItem( 'resultados', JSON.stringify(this.resultados));
+//       });
+
+//   }
+// }
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-  
 
+  private apiKey     : string = 'CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
 
-  private apiKey      : string = 'dr0k4vzB3ayXZPoj6opbHsWkk5JKMXf9';
-  // private servicioUrl : string = 'https://api.giphy.com/v1/gifs';
-  private _historial  : string [] = [];
+  public resultados: Gif[] = [];
 
-  //cambiar any por su tipo correspondiente
-  public resultados: Gif [] = [];
 
   get historial() {
     return [...this._historial];
   }
 
-  constructor( private htttp : HttpClient){
+  constructor( private http: HttpClient ) {
 
     this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
     this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
-    // if(localStorage.getItem('historial')){
-    //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+  
+
+    // if( localStorage.getItem('historial') ){
+    //   this._historial = JSON.parse( localStorage.getItem('historial')! );
     // }
 
   }
 
-  buscarGifs(query : string){
+
+
+  buscarGifs( query: string = '' ) {
 
     query = query.trim().toLocaleLowerCase();
     
+    if( !this._historial.includes( query ) ) {
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0,10);
 
-    if( !this._historial.includes (query) ){
-
-      this._historial.unshift( query ) ;
-      this._historial = this._historial.splice(0,10)
-
-      localStorage.setItem( 'historial', JSON.stringify(this._historial));
-      
+      localStorage.setItem('historial', JSON.stringify( this._historial )  );
     }
-    
+
     const params = new HttpParams()
-          .set('api_Key', this.apiKey)
+          .set('api_key', this.apiKey)
           .set('limit', '10')
-          .set('q', query);
+          .set('q', query );
 
-    this.htttp.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=dr0k4vzB3ayXZPoj6opbHsWkk5JKMXf9&q=${ query }&limit=10`)
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params } )
       .subscribe( ( resp ) => {
-        console.log( resp.data );
         this.resultados = resp.data;
-        localStorage.setItem( 'resultados', JSON.stringify(this.resultados));
-      })
-
-    
+        localStorage.setItem('resultados', JSON.stringify( this.resultados )  );
+      });
 
   }
+  
+
 
 }
